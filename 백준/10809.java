59e194487b4a2b6d9011769ce296591cc61b0540
hana4979 <o3o_s2__@naver.com>
2022-02-22 21:52:19 +0900

    220222

--- 백준/10809.java
+++ 백준/10809.java
@@ -1,13 +1,12 @@
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException{
 		
-		String[] alphabet = {"a", "b", "c", "d", "h", "i", "j", "k", "l", "m", "n",
-							 "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+		String[] alphabet = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+							"n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
 		
 		int[] firstArr = new int[26]; // 알파벳 26개
 		
@@ -15,31 +14,24 @@ public class Main {
 		
 		String word = bf.readLine(); // 단어 입력 받기
 		
-		// **error** StringTokenizer로 공백없이 받는 법 찾기
-		StringTokenizer st = new StringTokenizer(word, "");
+		// *** toCharArr() 이용해서 문자열을 한 글자시 char형으로 바꿀 수도 있음
+		String[] wordArr = word.split(""); // 공백없이 한 글자씩 자르기
 		
 		// firstArr값을 모두 -1로 설정
 		for(int i = 0; i < firstArr.length; i++)
 			firstArr[i] = -1;
 		
-		String token; // 토큰을 임시저장할 변수
-		int count = 0;
-		
-		do {
-			token = st.nextToken();
-			
-			System.out.println(token);
-			
-			for(int i = 0; i < alphabet.length; i++) {
-				if(alphabet[i].equals(token)) { // 토큰 값이 해당 알파벳이고
-					if(firstArr[i] == -1) { // 이전에 나온 알파벳이 아니라면
-						firstArr[i] = count;
-						count++;
+		for(int i = 0; i < wordArr.length; i++) {
+			for(int j = 0; j < alphabet.length; j++) {
+				if(alphabet[j].equals(wordArr[i])) { // 토큰 값이 해당 알파벳이고
+					if(firstArr[j] == -1) { // 이전에 나온 알파벳이 아니라면
+						firstArr[j] = i;
+						break; // 입력을 완료했다면 빠져나가기
 					}
 				}
 			}
-		} while(st.hasMoreTokens()); // 토큰이 존재할 때까지 계속 돌림
-		
+		}
+
 		for(int i = 0; i < firstArr.length; i++)
 			System.out.print(firstArr[i] + " ");
 	}
