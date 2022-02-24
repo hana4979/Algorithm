c1b4d8a6ec92bda215a574a1a283f4a838277f13
hana4979 <o3o_s2__@naver.com>
2022-02-24 23:26:23 +0900

    220224

---
+++ 백준/1157.java
@@ -0,0 +1,45 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		
+		String str = bf.readLine(); // 단어 입력 받기
+		str = str.toUpperCase(); // 모두 대문자로 변환
+		
+		int[] alphabetArr = new int[26]; // 알파벳 26개
+		char[] charArr = str.toCharArray(); // char형으로, 하나씩 배열에 저장
+		
+		for(int i = 0; i < charArr.length; i++) {
+			for(int j = 0; j < alphabetArr.length; j++) {
+				if((int)(charArr[i] - 65) == i) { // char형 대문자 A - 65 = 0
+					alphabetArr[i] += 1; // 빈도수 1 증가
+					break;
+				}
+			}
+		}
+		
+		int max = alphabetArr[0];
+		char alphabet = 0;
+		int second = 0;
+		
+		for(int i = 1; i < alphabetArr.length; i++) {
+			if(alphabetArr[i] >= max) {
+				second = max;
+				max = alphabetArr[i];
+				alphabet = (char)(i + 65); // 대문자로 저장(후에 변환)
+			}
+		}
+		
+		if(max == second)
+			System.out.println("?");
+		else
+			System.out.println(alphabet);
+		
+	}
+}
+
+// 뭐가 문제냐.. 낼 다시 보기 (220224)
