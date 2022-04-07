071df6e24efd94978d119618e7ae0aea16bacd16
hana4979 <o3o_s2__@naver.com>
2022-04-07 23:32:04 +0900

    220407

--- 백준/11650.java
+++ 백준/11650.java
@@ -3,44 +3,40 @@ import java.io.InputStreamReader;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.StringTokenizer;
+import java.util.Comparator;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		
+
 		int n = Integer.parseInt(bf.readLine());
-		
-		int[] x = new int[n];
-		int[] y = new int[n];
-		
-		for(int i = 0; i < n; i++) {
+
+		int[][] coor = new int[n][2];
+
+		for (int i = 0; i < n; i++) {
 			StringTokenizer str = new StringTokenizer(bf.readLine(), " ");
-			x[i] = Integer.parseInt(str.nextToken());
-			y[i] = Integer.parseInt(str.nextToken());
+			coor[i][0] = Integer.parseInt(str.nextToken()); // x좌표 저장
+			coor[i][1] = Integer.parseInt(str.nextToken()); // y좌표 저장
 		}
 		
-		Arrays.sort(x); // 문제1. 먼저 정렬을 해버리면 이후 x[i]와 y[i]는 꼬임
+		// 2차원 배열 정렬 (다중 배열 정렬)
+		Arrays.sort(coor, new Comparator<int[]>() {
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				if (o1[0] == o2[0]) // 0번째 열이 같으면
+					return o1[1] - o2[1]; // 1번째 열로 비교
+				else
+					return o1[0] - o2[0]; // 다르다면 0번째 열로 비교
+			}
+		});
 		
 		for(int i = 0; i < n; i++) {
-			for(int j = 1; j < n; j++) {
-				if(x[i] == x[j]) {
-					if(y[i] > y[j])
-						System.out.println(x[i] + " " + y[i]);
-					else // y[i] < x[i]
-						System.out.println(x[j] + " " + y[j]);
-				} else
-					System.out.println(x[i] + " " + y[i]);
-			}
+			System.out.println(coor[i][0] + " " + coor[i][1]);
 		}
-		
-		/*
-		public static void swap(int[] y) { // swap 함수로 y 배열 위치 조정
-			
-		}
-		*/
-		
+
 	}
 }
 
 // 220405 x와 y를 한 번에 받는 2차원 배열로 다시 풀기. 급히 내야하는 과제가 있어서 다시 시도 못 함 ㅜㅜ
+// 220407 다중 배열 정렬이라는 것이 있다더라.
