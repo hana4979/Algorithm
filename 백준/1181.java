b5b79d53890e89fda22fbb3c7dc24dc60fc03ed7
hana4979 <o3o_s2__@naver.com>
2022-04-13 23:57:43 +0900

    220413

--- 백준/1181.java
+++ 백준/1181.java
@@ -14,25 +14,47 @@ public class Main {
 
 		int n = Integer.parseInt(bf.readLine());
 		
-		// 문제 1. char 로 받으면 string 을 변환해 넣을 방법이 없음
-		// 문제 2. 구조체 형태로 받고 싶은데 배열 크기를 n만큼 지정 못 하겠음, 크기 지정한다 해도 배열을 어떻게 불러오지?
-		String[] strArr = new String[n];
-		int[] lenArr = new int[n];
+		String[][] strArr = new String[n][2];
+		int[][] lenArr = new int[n][2];
 		
 		for(int i = 0; i < n; i++) {
-			strArr[i] = bf.readLine();
-			lenArr[i] = strArr[i].length();
+			strArr[i][0] = bf.readLine();
+			strArr[i][1] = String.valueOf(i);
+			
+			lenArr[i][0] = strArr[i][0].length();
+			lenArr[i][1] = i;
 		}
 		
-		//문제 3. 일단 따로따로 해보려 했는데 생각해보니 글자크기만으로는 무슨 글자인지 불러올 수가 없다
-		Arrays.sort(lenArr); // 글자크기 순 배열
-		Arrays.sort(strArr); // 사전 순 배열
+		Arrays.sort(lenArr);
 		
-		for(String i : strArr) {
-			System.out.println(i);
+		// 배열 하나 더 생성한 뒤 해당 값 넣는 걸로
+		String sortArr[] = new String[n];
+		
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n; j++) {
+				if(lenArr[i][1] == Integer.parseInt(strArr[j][1])) {
+					sortArr[i] = strArr[j][0];
+				}
+			}
 		}
+		
+		for(String str : sortArr)
+			System.out.println(str);
+		
+		
+		/* 이건 아닌 듯 ㅡㅡ
+		// 1. 두 배열 다 행 정렬
+		Arrays.sort(strArr); // 사전 순
+		Arrays.sort(lenArr); // 글자 크기 순
+		
+		// 2. 글자 기준으로 비교, 같은 게 있다면 strArr에 int로 변환한 열값 찾아내서 정렬시도
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n; j++)
+			if(strArr[i][0].length() == lenArr[j][0]) {
+				
+			}
+		}
+		*/
 
 	}
 }
-
-// 220412 클래스로 한 번 시도해보기
