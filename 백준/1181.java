97005dbe5f384ed5a745ed28c2a00a31ceb3165b
hana4979 <o3o_s2__@naver.com>
2022-04-14 23:47:04 +0900

    220414

--- 백준/1181.java
+++ 백준/1181.java
@@ -2,10 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 import java.util.Arrays;
-
-class strArr{
-	
-}
+import java.util.Comparator;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
@@ -25,36 +22,34 @@ public class Main {
 			lenArr[i][1] = i;
 		}
 		
-		Arrays.sort(lenArr);
+		// 길이가 짧은 것부터 --> 사전 순 먼저 배열 하기
+		// 문제1 : String은 Comparator했을 때 뭘 반환해야 할까
+		Arrays.sort(strArr);
+		/*
+		 * Arrays.sort(strArr, new Comparator<String[]>() {
+			@Override
+			public int compare(String[] o1, String[] o2) {
+				return o1[0] - o2[0];
+			}
+		});
+		*/
 		
 		// 배열 하나 더 생성한 뒤 해당 값 넣는 걸로
 		String sortArr[] = new String[n];
 		
 		for(int i = 0; i < n; i++) {
 			for(int j = 0; j < n; j++) {
-				if(lenArr[i][1] == Integer.parseInt(strArr[j][1])) {
+				if(Integer.parseInt(strArr[i][1]) == lenArr[j][1]) {
 					sortArr[i] = strArr[j][0];
+					break;
 				}
 			}
 		}
 		
+		Arrays.sort(sortArr);
+		
 		for(String str : sortArr)
 			System.out.println(str);
-		
-		
-		/* 이건 아닌 듯 ㅡㅡ
-		// 1. 두 배열 다 행 정렬
-		Arrays.sort(strArr); // 사전 순
-		Arrays.sort(lenArr); // 글자 크기 순
-		
-		// 2. 글자 기준으로 비교, 같은 게 있다면 strArr에 int로 변환한 열값 찾아내서 정렬시도
-		for(int i = 0; i < n; i++) {
-			for(int j = 0; j < n; j++)
-			if(strArr[i][0].length() == lenArr[j][0]) {
-				
-			}
-		}
-		*/
 
 	}
 }
