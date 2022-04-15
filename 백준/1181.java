751d5d8ffc03c4b0ed76f135ba90a7bb02422abb
hana4979 <o3o_s2__@naver.com>
2022-04-15 23:08:22 +0900

    220415

--- 백준/1181.java
+++ 백준/1181.java
@@ -17,39 +17,47 @@ public class Main {
 		for(int i = 0; i < n; i++) {
 			strArr[i][0] = bf.readLine();
 			strArr[i][1] = String.valueOf(i);
-			
+		}
+		
+		// 길이가 짧은 것부터 --> 사전 순 먼저 배열 하기
+		Arrays.sort(strArr, new Comparator<String[]>() {
+			@Override
+			public int compare(String[] o1, String[] o2) {
+				return o1[0].toString().compareTo(o2[1].toString());
+			}
+		});
+		
+		// 사전 순 정렬을 기반으로 길이 순대로 lenArr에 넣기.. 가 안 됨.
+		// if를 건들여야 되나
+		for(int i = 0; i < n; i++) {
 			lenArr[i][0] = strArr[i][0].length();
 			lenArr[i][1] = i;
 		}
 		
-		// 길이가 짧은 것부터 --> 사전 순 먼저 배열 하기
-		// 문제1 : String은 Comparator했을 때 뭘 반환해야 할까
-		Arrays.sort(strArr);
-		/*
-		 * Arrays.sort(strArr, new Comparator<String[]>() {
+		Arrays.sort(lenArr, new Comparator<int[]>() {
 			@Override
-			public int compare(String[] o1, String[] o2) {
+			public int compare(int[] o1, int[] o2) {
 				return o1[0] - o2[0];
 			}
 		});
-		*/
 		
 		// 배열 하나 더 생성한 뒤 해당 값 넣는 걸로
 		String sortArr[] = new String[n];
 		
 		for(int i = 0; i < n; i++) {
 			for(int j = 0; j < n; j++) {
-				if(Integer.parseInt(strArr[i][1]) == lenArr[j][1]) {
+				if(lenArr[i][1] == Integer.parseInt(strArr[j][1])) {
 					sortArr[i] = strArr[j][0];
 					break;
 				}
 			}
 		}
 		
-		Arrays.sort(sortArr);
-		
 		for(String str : sortArr)
 			System.out.println(str);
 
 	}
 }
+
+// 1. 사전 순 배열 기반으로 글자 수대로 정렬이 안 됨
+// 220415 하 일단 시험 끝나고 다시 해보는 걸로 도저히 모르겠다
