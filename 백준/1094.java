2206b780b54bb435e886b9669e1e0804414b9925
hana4979 <o3o_s2__@naver.com>
2022-05-10 23:36:01 +0900

    220510

--- 백준/1094.java
+++ 백준/1094.java
@@ -4,24 +4,33 @@ import java.io.IOException;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		
+
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		
+
 		int want = Integer.parseInt(bf.readLine()); // 만들고자 하는 막대기 길이
 		int stick = 64; // 64cm의 막대기
 		int sumStick = 0;
-		int count = 1;
-		
-		while(sumStick != want) {
-			stick /= 2; // 1. 가장 짧은 막대기를 절반으로 자른다
-			sumStick += stick;
-			if(stick >= want) // 2. 자르고 남은 막대기가 want 보다 길거나 같다면
-				sumStick -= stick; // 2-1. 반은 버려버린다
-			count++;
+		int count = 0;
+
+		while (sumStick != want) {
+
+			if (want == 64) {
+				count++;
+				break; // 만들고자 하는 막대기가 입력받은 값인 64인 경우
+			}
+			
+			stick /= 2; // 가장 짧은 막대기를 절반으로 자른다
+			if (sumStick + stick <= want) { // 자르고 남은 막대기를 더한 값이 want보다 작거나 같다면
+				sumStick += stick; // 반을 더한다
+				count++;
+			}
+			
+			if(sumStick + stick <= want && sumStick + stick != 64) { // 남은 막대기가 원하는 길이가 아니라면
+				sumStick += stick;
+				count++;
+			}
 		}
-		
+
 		System.out.println(count);
 	}
 }
-
-// 220503 sumStick + stick 을 해야 조건에 부합하는 경우, sumStick 만으로 조건이 부합되는 경우, 아예 while을 빠져나오지 못 하는 경우 3가지 존재 --> 알고리즘 재분석 
