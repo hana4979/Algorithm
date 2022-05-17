a117de48416d329ad3caab9d8c0ace5830a01266
hana4979 <o3o_s2__@naver.com>
2022-05-17 22:00:36 +0900

    220517

--- 백준/1919.java
+++ 백준/1919.java
@@ -11,22 +11,38 @@ public class Main {
 		int[] wordArr2 = new int[26];
 		int removeCount = 0; // 제거해야 할 최소 갯수 문자
 		
-		String word = bf.readLine();
-		//StringTokenizer token = new StringTokenizer(word, ""); 굳이 X
-		for(int i = 0; i < word.length(); i++ ) {
-			int alphabet = word.charAt(i) - 97; // 아스키 코드 97 == a
-			for (int j = 0; j < 26; j++) {
-				if (j == alphabet) {
-					wordArr1[j]++;
-				}
+		/*
+		for (int i = 0; i < 2; i++) {
+			String word = bf.readLine();
+			for (int j = 0; j < word.length(); j++) {
+				int alphabet = word.charAt(j) - 97; // 아스키 코드 97 == a
+				// 매번 확인해야하는 불편함 존재
+				if (i == 0)
+					wordArr1[alphabet]++;
+				else
+					wordArr2[alphabet]++;
 			}
-			
 		}
+		*/
 		
-		// 비교
+		// 짧게 줄인 코드
+		String word1 = bf.readLine();
+		for(int i = 0; i < word1.length(); i++)
+			wordArr1[word1.charAt(i) - 97]++;
+
+		String word2 = bf.readLine();
+		for(int i = 0; i < word2.length(); i++)
+			wordArr2[word2.charAt(i) - 97]++;
 		
-		// 출력
-		
+		for (int i = 0; i < wordArr1.length; i++) {
+			if (wordArr1[i] != wordArr2[i]) {
+				removeCount += Math.abs(wordArr1[i] - wordArr2[i]);
+			}
+		}
+
+		System.out.println(removeCount);
+
 	}
 }
-// 220516 같은 코드 이용하여 wordArr2 에도 값 넣기 + 비교 + 출력
+
+// 배열 하나만 사용하는 방법도 존재 (++ / -- 로 값 조정 후 절대값 추출)
