0234aed1c2e8100a94ab299db2c2fc1ea8a2ae63
hana4979 <o3o_s2__@naver.com>
2022-07-12 23:16:55 +0900

    220712

--- 백준/1159.java
+++ 백준/1159.java
@@ -1,35 +1,30 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.Arrays;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-
+		boolean playerOk = false;
 		int n = Integer.parseInt(bf.readLine());
 		int[] firstname = new int[26];
-		int[] copyFirstname = new int[26];
 		String name = "";
 
 		for (int i = 0; i < n; i++) {
 			name = bf.readLine();
 			firstname[name.charAt(0) - 97]++;
-			copyFirstname[name.charAt(0) - 97]++;
 		}
 
-		// 배열 정렬
-		Arrays.sort(copyFirstname);
-		int max = copyFirstname[copyFirstname.length - 1]; // 최댓값 저장
-
-		if (max < 5)
-			System.out.println("PREDAJA");
-		else {
-			for (int i = 0; i < firstname.length; i++) {
-				if (firstname[i] == max)
-					System.out.print((char)(i + 97));
+		for (int i = 0; i < firstname.length; i++) {
+			if (firstname[i] >= 5) {
+				System.out.print((char) (i + 97));
+				if (playerOk == false)
+					playerOk = true;
 			}
 		}
+
+		if (playerOk == false)
+			System.out.println("PREDAJA");
 	}
 }
