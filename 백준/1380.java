94abccb07598143b7221466430648e74a6c45939
hana4979 <o3o_s2__@naver.com>
2022-07-18 23:59:44 +0900

    220718

---
+++ 백준/1380.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(bf.readLine());
+
+		do {
+			// 귀걸이 뺏긴 학생 정보를 담을 HashMap
+			Map<Integer, String> student = new HashMap<Integer, String>();
+
+			for (int i = 0; i < n; i++) {
+				student.put(i + 1, bf.readLine()); // HashMap에 귀걸이 뺏긴 학생 이름 저장
+			}
+
+			int[] earring = new int[n];
+			int studentNumber = 0;
+
+			for (int i = 0; i < 2 * n - 1; i++) {
+				StringTokenizer token = new StringTokenizer(bf.readLine(), " ");
+
+				studentNumber = Integer.parseInt(token.nextToken());
+				earring[studentNumber - 1] += token.nextToken().charAt(0);
+
+				// 배열 값이 도합 131이 되면 HashMap에서 삭제
+				// 'A' + 'B' = 65 + 66 = 131
+				if (earring[studentNumber - 1] == 131) {
+					student.remove(studentNumber);
+				}
+			}
+
+			// 출력형식 손 보기. HashMap에 값이 있는 만큼 돌리는 게 아닌 듯.
+			int studentCount = 0; // 귀걸이를 돌려받지 못한 학생 수 세기
+			for (Map.Entry<Integer, String> entry : student.entrySet()) {
+				System.out.println(studentCount + " " + entry.getValue());
+			}
+
+			n = Integer.parseInt(bf.readLine());
+		} while (n != 0); // 입력에 0이 들어오면 반복문 종료
+
+	}
+}
