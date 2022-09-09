7058e3252afff4b8c87b3f8131c2acaf3681b414
hana4979 <o3o_s2__@naver.com>
2022-09-09 22:20:23 +0900

    220909

--- 백준/3059.java
+++ 백준/3059.java
@@ -13,7 +13,8 @@ public class Main {
 
 			boolean[] alphabet = new boolean[26];
 			String str = bf.readLine();
-			char result = ' ';
+			// *** result = ' ' --> 띄어쓰기 == 32
+			char result = 0;
 
 			for (int j = 0; j < str.length(); j++) {
 				alphabet[str.charAt(j) - 65] = true; // 문자에 'a'값을 뺀 배열의 인덱스 값을 true로 설정
@@ -21,12 +22,13 @@ public class Main {
 
 			for (int j = 0; j < alphabet.length; j++) {
 				if (!alphabet[j] == true) {
-					result += (char) j + 'a';
+					// *** (char) j + 'a' --> j 문자를 char 형으로 변환 
+					result += (char)(j + 65);
 				}
 			}
 
-			// 값이 ? 로 나오는 오류
-			System.out.println(result);
+			// *** char 형으로 출력시 ? 출력
+			System.out.println((int)result);
 		}
 
 	}
