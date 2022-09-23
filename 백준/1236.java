285f0644f161dd03b41968b80763abfcea773360
hana4979 <o3o_s2__@naver.com>
2022-09-23 23:51:08 +0900

    220923

--- 백준/1236.java
+++ 백준/1236.java
@@ -2,52 +2,60 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 import java.util.StringTokenizer;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
-		StringTokenizer token = new StringTokenizer(bf.readLine(), " ");
+		int scenario = 0; // 시나리오는 1번부터
 
-		int n = Integer.parseInt(token.nextToken()); // 성의 세로 크기
-		int m = Integer.parseInt(token.nextToken()); // 성의 가로 크기
+		do {
+			StringTokenizer weightToken = new StringTokenizer(bf.readLine());
 
-		char[][] castleGuard = new char[n][m];
+			int properWeight = Integer.parseInt(weightToken.nextToken()); // 적정 체중
+			int nowWeight = Integer.parseInt(weightToken.nextToken()); // 실제 체중
 
-		int rowGuardCount = 0; // 행에 필요한 경비원 수
-		int colGuardCount = 0; // 열에 필요한 경비원 수
+			if (properWeight == 0 && nowWeight == 0)
+				break;
 
-		// 배열에 값 저장하기 위한 for문
-		for (int i = 0; i < n; i++) {
-			String guardCheck = bf.readLine();
-			boolean rowGuard = false; // 행(가로)에 경비원 확인
+			String button = ""; // 운동 or 먹이 or "#" (종료)
+			int number = 0; // 체중 변화량
+			boolean ripTrue = false;
 
-			// 배열에 값 저장 && 행에 경비원 수 확인
-			for (int j = 0; j < m; j++) {
-				castleGuard[i][j] = guardCheck.charAt(j);
-				if (guardCheck.charAt(j) == 'X')
-					rowGuard = true;
+			// "# 0" 이 입력될 때까지 시나리오 반복
+			do {
+				StringTokenizer statusToken = new StringTokenizer(bf.readLine());
 
-				// 마지막까지 돌았을 때 경비원이 없었다면
-				if (j == m - 1 && rowGuard == false)
-					rowGuardCount++;
+				button = statusToken.nextToken();
+				number = Integer.parseInt(statusToken.nextToken());
+
+				if (button.equals("F")) {
+					nowWeight += number;
+				} else if (button.equals("E")) {
+					nowWeight -= number;
+				}
+				
+				// 펫이 죽더라도 입력은 계속 받아야 함
+				// 체중이 0 이하로 떨어진다면 RIP
+				if (nowWeight <= 0)
+					ripTrue = true;
+
+			} while (!(button.equals("#") && number == 0));
+
+			scenario++;
+
+			if (ripTrue == true) // 한 번이라도 체중이 0 이하로 떨어졌다면
+				System.out.println(scenario + " " + "RIP");
+			else if (nowWeight > (properWeight / 2) && nowWeight < (properWeight * 2)) {
+				System.out.println(scenario + " " + ":-)");
+			} else {
+				System.out.println(scenario + " " + ":-(");
 			}
-		}
 
-		// 열에 경비원 수 확인을 위한 for문
-		for (int i = 0; i < m; i++) {
-			for (int j = 0; j < n; j++) {
-				if (castleGuard[j][i] == 'X')
-					break;
-				if (j == n - 1) // 마지막까지 돌았을 때 경비원('X')이 없었다면
-					colGuardCount++;
-			}
-		}
+		} while (true);
 
-		if (rowGuardCount >= colGuardCount)
-			System.out.println(rowGuardCount);
-		else
-			System.out.println(colGuardCount);
 	}
 }
