16f90f22e2b5d23c88321e4db15b4115af743028
hana4979 <o3o_s2__@naver.com>
2022-10-14 18:44:18 +0900

    221014

--- 백준/1758.java
+++ 백준/1758.java
@@ -18,11 +18,12 @@ public class Main {
 		Collections.sort(tipList); // 오름차순 정렬
 		Collections.reverse(tipList); // 역순으로 정렬 == 내림차순 정렬
 
-		int tip_result = 0; // 총 팁
+		long tip_result = 0; // 등수 <= 100,000 , 팁 <= 100,000
 		for (int i = 0; i < customer; i++) {
-			int tip = tipList.get(i) - (i + 1 - 1); // 원래 주려고 생각했던 돈 - (받은 등수 - 1)
-			if (tip > 0) // tip이 양수라면(음수가 아니라면)
-				tip_result += tip; // 받을 수 있다
+			if (tipList.get(i) - i <= 0) // 내림차순 된 tip 값에서 등수를 뺏을 떄 0 이하라면
+				break; // 반복문 종료
+
+			tip_result += tipList.get(i) - (i + 1 - 1); // 원래 주려고 생각했던 돈 - (받은 등수 - 1)
 		}
 
 		System.out.println(tip_result);
