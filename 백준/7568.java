d5e2846a4f66b80e52dc4b4982958f8fb142d12a
hana4979 <o3o_s2__@naver.com>
2022-12-27 23:53:05 +0900

    221227

--- 백준/7568.java
+++ 백준/7568.java
@@ -3,28 +3,37 @@ import java.io.InputStreamReader;
 import java.io.IOException;
 import java.util.StringTokenizer;
 import java.util.Arrays;
-import java.util.HashMap;
+import java.util.ArrayList;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
-		HashMap<Integer, Integer> size = new HashMap<Integer, Integer>();
-
 		int n = Integer.parseInt(bf.readLine());
+		int[][] peopleSize = new int[n][1];
 
 		for (int i = 0; i < n; i++) {
 			StringTokenizer token = new StringTokenizer(bf.readLine(), " ");
-			size.put(Integer.parseInt(token.nextToken()), Integer.parseInt(token.nextToken()));
+			peopleSize[i][0] = Integer.parseInt(token.nextToken()); // 무게 입력
+			peopleSize[i][1] = Integer.parseInt(token.nextToken()); // 키 입력
 		}
 
+		// 정렬 후 순서대로 rank 부여, peopleSize 와 비교해 기존 배열 순서대로 rank 나열
+		ArrayList<int[][]> compareSize = new ArrayList<int[][]>();
+
+		int[] rank = new int[n];
 		// 덩치 비교(해야됨)
-		int weight = size.getValue(0);
+		int weight = peopleSize[0][0];
 		int ranking = 1;
 		for (int i = 1; i < n; i++) {
-			if (weight <= size.getValue(i))
-				;
-
+			if (weight <= peopleSize[i][0]) {
+				weight = peopleSize[i][0];
+				rank[i] = ranking;
+				ranking++;
+			} else {
+				// 이렇게하면안될것같다는느낌이왔는데시간이없네;-;
+				ranking++;
+			}
 		}
 
 	}
