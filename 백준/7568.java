aa96d3860579151a7f2ac145beb6f7d6328b2824
hana4979 <o3o_s2__@naver.com>
2022-12-28 23:11:06 +0900

    221228

--- 백준/7568.java
+++ 백준/7568.java
@@ -18,23 +18,33 @@ public class Main {
 			peopleSize[i][1] = Integer.parseInt(token.nextToken()); // 키 입력
 		}
 
-		// 정렬 후 순서대로 rank 부여, peopleSize 와 비교해 기존 배열 순서대로 rank 나열
-		ArrayList<int[][]> compareSize = new ArrayList<int[][]>();
-
-		int[] rank = new int[n];
-		// 덩치 비교(해야됨)
-		int weight = peopleSize[0][0];
-		int ranking = 1;
-		for (int i = 1; i < n; i++) {
-			if (weight <= peopleSize[i][0]) {
-				weight = peopleSize[i][0];
-				rank[i] = ranking;
-				ranking++;
-			} else {
-				// 이렇게하면안될것같다는느낌이왔는데시간이없네;-;
-				ranking++;
+		// 2차원 배열 복사
+		int[][] compareSize = new int[n][1];
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < 2; j++) {
+				compareSize[i][j] = peopleSize[i][j];
 			}
 		}
+		
+		// https://crazykim2.tistory.com/463 오류 왜 뜨냐 진짜 ㅡㅡ
+		// 정렬 후 순서대로 rank 부여, peopleSize 와 비교해 기존 배열 순서대로 rank 나열
+		Arrays.sort(compareSize, new Comparator<int[]>() {
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				// 첫번째 기준 오름차순, 두번째 기준 오름차순
+				return o1[0] != o2[0] ? o1[0] - o2[0] : o1[1] - o2[1];
+			}
+		});
 
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (compareSize[i][0] == peopleSize[j][0])
+					if (compareSize[i][1] == peopleSize[j][1])
+						System.out.print(j + " ");
+					else {
+						continue;
+					}
+			}
+		}
 	}
 }
