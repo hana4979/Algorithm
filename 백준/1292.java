2b8a3da88e08a8c35c445ee66170ff74366839c1
hana4979 <o3o_s2__@naver.com>
2023-01-06 20:10:54 +0900

    230106

--- 백준/1292.java
+++ 백준/1292.java
@@ -6,29 +6,30 @@ import java.util.StringTokenizer;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		
+
 		StringTokenizer numToken = new StringTokenizer(bf.readLine());
-		
-		int startNum = Integer.parseInt(numToken.nextToken());
-		int lastNum = Integer.parseInt(numToken.nextToken());
-		
-		int[] sequence = new int[lastNum + 1];
+
+		int startNum = Integer.parseInt(numToken.nextToken()); // 시작구간
+		int lastNum = Integer.parseInt(numToken.nextToken()); // 끝구간
+
+		int[] sequence = new int[lastNum];
 		int idx = 0;
-		
-		// 수열 만들기 (오류수정하기)
-		for(int i = 1; idx < lastNum; i++) {
-			for(int j = 1; j <= i; j++) {
-				sequence[idx] = i;
-				idx++;
+
+		// 수열 만들기
+		for (int i = 1; idx < lastNum; i++) {
+			for (int j = 1; j <= i; j++) {
+				if (idx == lastNum) // idx값이 끝 구간값에 도달하면
+					break; // 배열에 더 이상 값을 넣을 수 없음
+				sequence[idx++] = i;
 			}
 		}
-		
+
 		int result = 0;
 		// 시작 구간부터 끝 구간까지의 정수 더하기
-		for(int i = startNum; i <= lastNum; i++) {
+		for (int i = startNum - 1; i < lastNum; i++) {
 			result += sequence[i];
 		}
-		
+
 		System.out.println(result);
 	}
 }
