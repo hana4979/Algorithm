1d9a5b484e96c01bb31be0e9f73a7818b51ecdec
hana4979 <o3o_s2__@naver.com>
2023-01-10 22:55:50 +0900

    230110

--- 백준/1436.java
+++ 백준/1436.java
@@ -1,35 +1,24 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.String;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
 		int series = Integer.parseInt(bf.readLine());
-		int count = 1; // 첫번째 시리즈
+		int count = 0; // 시리즈
 		int num = 666; // 시리즈의 시작인 666
 
 		while (count != series) {
-			int check_number6 = 0; // 6이 나왔을 때 체크
-			int digitNum = (int) Math.log10(num) + 1; // 자릿수 체크
-			for (int i = 0; i <= (int) Math.log10(num) + 1; i++) {
-				if (num % 10 * digitNum == '6') { // 각 자리수에 6이 나오면
-					check_number6++;
-					digitNum--;
-					if (check_number6 == 3) { // 6이 연속 3번 나왔다면
-						count++;
-						break; // for문 종료
-					}
-				} else {
-					check_number6 = 0;
-				}
+			// contains() 메소드 : 특정 문자열이 포함되어 있는지 확인
+			if ((String.valueOf(num)).contains("666")) // 문자열로 변환한 숫자에 666이 있다면
+				count++; // count값을 증가시킨다
 
-				num++;
-			}
-
-			System.out.println(num);
+			num++; // 하나하나씩 비교하기 위함
 		}
 
+		System.out.println(num - 1);
 	}
 }
