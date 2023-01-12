d140f90c6c2db642bd60fe5dbf9e89bd94c310ee
hana4979 <o3o_s2__@naver.com>
2023-01-12 22:46:55 +0900

    230112

--- 백준/10815.java
+++ 백준/10815.java
@@ -10,21 +10,21 @@ public class Main {
 
 		// 상근이가 가지고 있는 숫자
 		int have = Integer.parseInt(bf.readLine());
+		HashSet<String> haveSet = new HashSet<>();
 
-		StringTokenizer haveToken = new StringTokenizer(bf.readLine());
-
-		// 주어진 숫자
-		HashSet<String> numSet = new HashSet<>();
-
-		// 헐 토큰 중복 저장 못해?
-		StringTokenizer numToken = new StringTokenizer(bf.readLine());
-
-		while (numToken.hasMoreTokens()) { // token이 떨어질 때까지 집합에 값 넣기
-			numSet.add(numToken.nextToken());
-		}
+		StringTokenizer haveToken = new StringTokenizer(bf.readLine(), " ");
 
 		for (int i = 0; i < have; i++) {
-			if (numSet.contains(haveToken.nextToken())) { // set에 값이 포함되어 있다면
+			// 순서 상관없이 Set(집합)에 저장만 되면 됨
+			haveSet.add(haveToken.nextToken());
+		}
+
+		// 주어진 숫자
+		int num = Integer.parseInt(bf.readLine());
+		StringTokenizer numToken = new StringTokenizer(bf.readLine(), " ");
+
+		for (int i = 0; i < num; i++) {
+			if (haveSet.contains(numToken.nextToken())) { // 주어진 숫자가 가지고 있는 숫자에 포함되어 있다면
 				System.out.print(1 + " "); // 1 출력
 			} else {
 				System.out.print(0 + " "); // 0 출력
