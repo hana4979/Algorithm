a5cc68172f1c4540fe652a6d07bd8a9ef5417677
hana4979 <o3o_s2__@naver.com>
2023-01-18 23:45:31 +0900

    230118

--- 백준/1620.java
+++ 백준/1620.java
@@ -8,30 +8,27 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
-		HashMap<Integer, String> poketmon = new HashMap<Integer, String>();
+		// keySet() 사용 시 시간초과 문제로 HashMap 2개 사용
+		HashMap<Integer, String> poketmonMap1 = new HashMap<Integer, String>();
+		HashMap<String, Integer> poketmonMap2 = new HashMap<String, Integer>();
 
 		StringTokenizer token = new StringTokenizer(bf.readLine());
 		int n = Integer.parseInt(token.nextToken()); // 도감에 수록되어 있는 포켓몬의 개수
 		int m = Integer.parseInt(token.nextToken()); // 맞춰야 하는 문제의 개수
 
 		for (int i = 1; i <= n; i++) {
-			poketmon.put(i, bf.readLine());
+			String poketmon = bf.readLine();
+			poketmonMap1.put(i, poketmon);
+			poketmonMap2.put(poketmon, i);
 		}
 
 		for (int j = 0; j < m; j++) {
 			String information = bf.readLine();
-			if (poketmon.containsValue(information)) { // 포켓몬 이름을 입력 받았다면
-				// keySey()을 이용해 key 출력
-				for (Integer key : poketmon.keySet()) {
-					if (poketmon.get(key).equals(information)) { // value값이 information과 같다면
-						System.out.println(key); // key값 출력
-					}
-				}
-			} else if (poketmon.containsKey(Integer.parseInt(information))) { // 숫자를 입력 받았다면
-				System.out.println(poketmon.get(Integer.parseInt(information))); // 포켓몬 이름 출력
+			if (poketmonMap2.containsKey(information)) { // 포켓몬 이름을 입력 받았다면
+				System.out.println(poketmonMap2.get(information)); // 숫자 출력
+			} else if (poketmonMap1.containsKey(Integer.parseInt(information))) { // 숫자를 입력 받았다면
+				System.out.println(poketmonMap1.get(Integer.parseInt(information))); // 포켓몬 이름 출력
 			}
 		}
 	}
 }
-
-// 시간초과
