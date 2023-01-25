d4a619c2d1390151917b6072ed7a6c92546ec71d
hana4979 <o3o_s2__@naver.com>
2023-01-25 23:57:00 +0900

    230125

--- 백준/10816.java
+++ 백준/10816.java
@@ -12,35 +12,45 @@ public class Main {
 
 		// 상근이가 가지고 있는 숫자 카드
 		int n = Integer.parseInt(bf.readLine());
-		int[] haveArr = new int[n];
+		String[] haveArr = new String[n];
 
 		StringTokenizer haveToken = new StringTokenizer(bf.readLine());
 
 		for (int i = 0; i < n; i++)
-			haveArr[i] = Integer.parseInt(haveToken.nextToken());
-
-		Arrays.sort(haveArr); // 정렬로 비교하기 쉽도록 함
-
+			haveArr[i] = haveToken.nextToken();
+		
 		// 주어진 숫자 카드
 		int m = Integer.parseInt(bf.readLine());
-		int[] givenArr = new int[m]; // 숫자와 빈도수를 저장할 2차원 배열
+		Integer[][] givenArr = new Integer[m][1]; // 빈도수까지 저장할 2차원 배열
 
 		StringTokenizer givenToken = new StringTokenizer(bf.readLine());
 
 		for (int i = 0; i < m; i++)
-			givenArr[i] = Integer.parseInt(givenToken.nextToken());
+			givenArr[i][0] = Integer.parseInt(givenToken.nextToken());
 
-		// Cannot infer type arguments for
-		ArrayList<Integer> haveList = new ArrayList<>(Arrays.asList(haveArr));
+		// .contains() 사용을 위해
+		ArrayList<String> haveList = new ArrayList<>(Arrays.asList(haveArr));
 
-		for (int i = 0; i < n; i++) {
-			int count = 0;
-			while (haveList.contains(givenArr[i])) {
-				count++;
+		for (int i = 0; i < m; i++) {
+			Integer num = givenArr[i][0];
+			
+			if(haveList.contains(num)) {
+				freq(givenArr, num);
 			}
-
-			System.out.print(count + " ");
 		}
-
+		
+		// 출력
+		for(int i = 0; i < m; i++) {
+			System.out.print(givenArr[i][1] + " ");
+		}
+	}
+	
+	// 각 숫자의 빈도수 알아보기
+	public static void freq(Integer[][] givenArr, Integer str) {
+		for(int i = 0; i < givenArr.length; i++) { // row(행)만큼 조사
+			if(givenArr[i][0].equals(str)) {
+				givenArr[i][1]++;
+			}
+		}
 	}
 }
