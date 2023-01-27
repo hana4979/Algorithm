8c0431b4876db87250ac15f71da6cda87bf92e9c
hana4979 <o3o_s2__@naver.com>
2023-01-27 23:34:34 +0900

    230127

--- 백준/1764.java
+++ 백준/1764.java
@@ -3,7 +3,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 import java.util.HashSet;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
@@ -20,23 +21,20 @@ public class Main {
 
 		m = Math.max(n, m); // n과 m을 비교했을 때 더 큰 수를 m으로 지정
 
-		int count = 0;
-		String[] whoArr = new String[m];
+		ArrayList<String> whoList = new ArrayList<>(); // 듣보잡을 저장할 ArrayList
 
 		for (int i = 0; i < m; i++) {
 			String unsight = bf.readLine();
 			if (unhear.contains(unsight)) { // 듣도 못한 사람과 겹친다면
-				whoArr[i] = unsight;
-				count++;
+				whoList.add(unsight); // List에 저장
 			}
 		}
 
-		// java.lang.NullPointerException
-		Arrays.sort(whoArr);
-		System.out.println(count);
-
-		for (int i = 0; i < whoArr.length; i++) {
-			System.out.println(whoArr[i]);
+		// Arrays 정렬 시 java.lang.NullPointerException 오류가 떠서 ArrayList
+		Collections.sort(whoList);
+		System.out.println(whoList.size()); // 듣보잡의 수 출력
+		for (int i = 0; i < whoList.size(); i++) {
+			System.out.println(whoList.get(i));
 		}
 	}
 }
