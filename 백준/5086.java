4e7919312195d191b91cc59d47abef6d355749a3
hana4979 <o3o_s2__@naver.com>
2023-02-02 16:55:22 +0900

    230202

--- 백준/5086.java
+++ 백준/5086.java
@@ -6,23 +6,23 @@ import java.util.StringTokenizer;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		
-		while(true) {
+
+		while (true) {
 			StringTokenizer numToken = new StringTokenizer(bf.readLine());
-			
+
 			int first = Integer.parseInt(numToken.nextToken());
 			int second = Integer.parseInt(numToken.nextToken());
-			
-			if(first == 0 && second == 0) // 0 0 이 입력되면
+
+			if (first == 0 && second == 0) // 0 0 이 입력되면
 				break;
-			
-			if((second >= first) && (second % first == 0)) // 첫번째 숫자가 약수라면
+
+			if (second % first == 0) // 첫번째 숫자가 약수라면
 				System.out.println("factor");
-			else if((first >= second) && (first % second == 0)) // 첫번째 숫자가 배수라면
-				System.out.println("mutiple");
+			else if (first % second == 0) // 첫번째 숫자가 배수라면
+				System.out.println("multiple");
 			else
 				System.out.println("neither"); // 약수도, 배수도 아니라면
 		}
-		
+
 	}
 }
