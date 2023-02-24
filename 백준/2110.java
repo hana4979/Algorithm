9c0e9af37b0279182b4b52090b8a19757d47a2a3
hana4979 <o3o_s2__@naver.com>
2023-02-24 23:45:41 +0900

    230224

--- 백준/2110.java
+++ 백준/2110.java
@@ -8,7 +8,7 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
-		StringTokenizer token = new StringTokenizer(bf.readLine());
+		StringTokenizer token = new StringTokenizer(bf.readLine(), " ");
 
 		int n = Integer.parseInt(token.nextToken()); // 집의 개수
 		int c = Integer.parseInt(token.nextToken()); // 공유기 개수
@@ -24,16 +24,33 @@ public class Main {
 		// 인덱스 0, n-1에 공유기를 필수로 설치한다는 가정
 		int min = 1;
 		int max = n - 2;
+		int mid = 0;
+		int gab = 0; // 거리의 최대값을 확인할 gab
 		
-		// 230223 : 나름 생각 중...
 		while(min <= max) {
-			int mid = (max - min) / 2 + min;
+			mid = (max - min) / 2 + min;
+			gab = houseArr[max] - houseArr[min];
 			
-			if((houseArr[mid] - houseArr[0]) < (houseArr[max] - houseArr[mid])){
+			// 230224 거짓말아니고 도합 3시간동안 고민했는데 감도 안 잡힘
+			/*
+			 * 양 끝을 제외한 범위에 공유기 n - 2개가 설치될 수 있는지 확인
+			 * 설치될 수 있다면 값이 최대값인지 확인
+			 */
+			
+			if(){
 				mid = max + 1;
 			} else
 				mid = min - 1;
+			
+			/*
+			System.out.println("max : " + max);
+			System.out.println("min : " + min);
+			*/
 		}
+		
+		
+		
+		System.out.print(mid);
 		
 		
 		
