3b210581b0af24b764a74debd726d75cbeb5ed4c
hana4979 <o3o_s2__@naver.com>
2023-03-31 23:55:00 +0900

    230331

---
+++ 백준/7785.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		
+		HashMap<String, String> commute = new HashMap<>();
+		StringTokenizer token;
+		
+		int n = Integer.parseInt(bf.readLine());
+		
+		String name = ""; // 직원 이름
+		String state = ""; // 직원 출퇴근 여부
+		
+		for(int i = 0; i < n; i++) {
+			token = new StringTokenizer(bf.readLine(), " ");
+			
+			name = token.nextToken();
+			state = token.nextToken();
+			
+			if(state.equals("leave")) { // 퇴근 상태라면
+				commute.remove(name); // name과 같은 key값의 자료를 삭제
+				// commute.values().remove(value); 하면 값을 기준으로 삭제할 수 있음
+			} else { // 출근 상태라면
+				commute.put(name, state); // HashMap에 직원 자료 추가
+			}
+		}
+		
+		List<String> keySet = new ArrayList<>(commute.keySet());
+		
+		Collections.reverse(keySet); // key 역순 정렬
+		
+		for(String key : keySet) {
+			System.out.println(key);
+		}
+	}
+}
