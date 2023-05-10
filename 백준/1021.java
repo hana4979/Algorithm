9d868df853b51e9047f4415181cd0cf251c4ea57
hana4979 <o3o_s2__@naver.com>
2023-05-10 23:57:38 +0900

    230510

--- 백준/1021.java
+++ 백준/1021.java
@@ -10,7 +10,6 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
 		StringTokenizer token = new StringTokenizer(bf.readLine(), " ");
 
 		int N = Integer.parseInt(token.nextToken());
@@ -18,31 +17,26 @@ public class Main {
 
 		Deque<Integer> dq = new LinkedList<>();
 
-		token = new StringTokenizer(bf.readLine());
-
-		for (int i = 0; i < N; i++) {
-			dq.offer(Integer.parseInt(token.nextToken()));
-
-		}
+		for (int i = 1; i <= N; i++)
+			dq.offerLast(i);
 
 		token = new StringTokenizer(bf.readLine(), " ");
 		int pickNum = Integer.parseInt(token.nextToken());
 		int count = 0;
-		
+
 		while (token.hasMoreTokens()) {
-			if (dq.peek() == pickNum) {
-				dq.poll();
-				count++;
+			if (dq.peek() == pickNum) { // 뽑아야하는 숫자면
+				dq.poll(); // 제거 (후 리턴)
 				pickNum = Integer.parseInt(token.nextToken());
-				
-			} else if (dq.peek() > pickNum) {
-				dq.offerLast(dq.pollFirst());
+
+			} else if (dq.peek() > pickNum) { // 뽑아야하는 숫자가 맨 앞 숫자보다 작다면
+				dq.offerFirst(dq.pollLast()); // 오른쪽으로 이동
 				count++;
-				
-			} else {
-				dq.offerFirst(dq.poll());
+
+			} else { // 뽑아야하는 숫자가 맨 앞 숫자보다 크다면
+				dq.offerLast(dq.pollFirst()); // 왼쪽으로 이동
 				count++;
-				
+
 			}
 
 		}
@@ -50,3 +44,5 @@ public class Main {
 		System.out.println(count);
 	}
 }
+
+// 230510 : 예제 입력 3, 4 틀린 답 나옴
